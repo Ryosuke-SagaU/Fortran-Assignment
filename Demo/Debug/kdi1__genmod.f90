e809ef0acb27dd3c421f1c56a528666f1c577944
Ryosuke-SagaU <19238346@edu.cc.saga-u.ac.jp>
2022-06-17 19:55:34 +0900

    [develop] chnage Demo.f90 file

---
+++ Demo/Debug/kdi1__genmod.f90
@@ -0,0 +1,15 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Fri Jun 17 19:51:48 2022
+        MODULE KDI1__genmod
+          INTERFACE 
+            SUBROUTINE KDI1(TWSO,ETA,MWF,MWS,MCS,WP,QE,QC)
+              REAL(KIND=8), INTENT(IN) :: TWSO
+              REAL(KIND=8), INTENT(OUT) :: ETA
+              REAL(KIND=8), INTENT(OUT) :: MWF
+              REAL(KIND=8), INTENT(OUT) :: MWS
+              REAL(KIND=8), INTENT(OUT) :: MCS
+              REAL(KIND=8), INTENT(OUT) :: WP
+              REAL(KIND=8), INTENT(OUT) :: QE
+              REAL(KIND=8), INTENT(OUT) :: QC
+            END SUBROUTINE KDI1
+          END INTERFACE 
+        END MODULE KDI1__genmod
--- Demo/Demo.f90
+++ Demo/Demo.f90
@@ -14,83 +14,96 @@
 
     
     program Demo
-
-    implicit real(a-z)
     
+    implicit double precision(a-h,k-m,o-z)
+    implicit integer(i-j,n)
+    interface
+    subroutine KDI1(Twso,eta,mwf,mws,mcs,Wp,Qe,Qc)
+    real(8),intent(in)::Twso
+    real(8),intent(out)::eta,mwf,mws,mcs,Wp,Qe,Qc
+    end subroutine
+    end interface
     call KPAMES(3,1)
+    open(1,file='6.txt',status='unknown')
     
-    T1=28.0
+    do i=1,7
+        Twso=i+22.0
+        call KDI1(Twso, eta, mwf, mws, mcs, Wp, Qe, Qc)
+        write(1,*)
+        write(1,*)
+        write(1,*)
+        write(1,*)
+        write(1,*)
+        write(1,*)'i=',Twso
+        write(1,*)
+        write(1,*)'îMå¯ó¶'
+        write(1,*)'eta=',eta*100
+        write(1,*)
+        write(1,*)'çÏìÆó¨ëÃó¨ó '
+        write(1,*)'mwf=',mwf
+        write(1,*)
+        write(1,*)'â∑êÖó¨ó '
+        write(1,*)'mws=',mws
+        write(1,*)
+        write(1,*)'ó‚êÖó¨ó '
+        write(1,*)'mcs=',mcs
+    end do
     
-    open(2,file='point1.txt', status='unknown')
-    
-    p1 = PST(T1)
-    v1= VTDD(T1)
-    h1 = HTDD(T1)
-    s1 = STDD(T1)
-    
-    write(2,*) "1"
-    write(2,*) 'p1=',p1
-    write(2,*) 'v1=',v1
-    write(2,*) 'h1=',h1
-    write(2,*) 's1=',s1
-    
-    T2 = 4.0
-    s2 = s1
-    
-    p2 = PST(T2)
-    x2 = XPS(p2, s2)
-    h2 = HPX(p2,x2)
-    v2 = VPX(p2,x2)
-    
-    write(2,*) "2"
-    write(2,*) 'p2=',p2
-    write(2,*) 'x2=',x2
-    write(2,*) 'v2=',v2
-    write(2,*) 'h2=',h2
-    
-    T3 = 4.0
-    p3 = p2
-    
-    v3 = VTD(T3)
-    h3 = HTD(T3)
-    s3 = STD(T3)
-    
-    write(2,*) "3"
-    write(2,*) 'v3=',v3
-    write(2,*) 'h3=',h3
-    write(2,*) 'v3=',s3
-    
-    p4 = p1
-    s4 = s3
-    
-    T4 = TPS(p4, s4)
-    v4 = VPS(p4, s4)
-    h4 = HPS(p4, s4)
-    
-    write(2,*) "4"
-    write(2,*) 'T4=',T4
-    write(2,*) 'v4=',v4
-    write(2,*) 'h4=',h4
-    
-    hw = h1 - h2 - h4 + h3
-    hq = h1 - h4
-    n = hw / hq
-    write(2,*) "îMå¯ó¶"
-    write(2,*) 'n=',n
-    
-    mwf = 100
-    wt = mwf * (h1 - h2)
-    wp = mwf * (h4 - h3)
-    qe = mwf * (h1 - h4)
-    qc = mwf * (h2 - h3)
-    
-    write(2,*)"â€ëË2"
-    write(2,*)'wt=',wt
-    write(2,*)'wp=',wp
-    write(2,*)'qe=',qe
-    write(2,*)'qc=',qc
-    
-    close(2)
-
     stop
     end program Demo
+    
+    subroutine KDI1(Twso,eta,mwf,mws,mcs,Wp,Qe,Qc)
+    real(8),intent(in)::Twso
+    real(8),intent(out)::eta,mwf,mws,mcs,Wp,Qe,Qc
+    call KPAMES(3,1)
+    
+    TwsI=30.0
+    TcsI=5.0
+    Tcso=8.0
+    UAperQe=0.5
+    UAperQc=UAperQe
+    Wt=1000.0
+    cp=4.2
+    
+    dTImE=1/UAperQe
+    dTImC=1/UAperQc
+    
+    TE=(Twso*exp((TwsI-Twso)/dTImE)-TwsI)/(exp((TwsI-Twso)/dTImE)-1)
+    TC=(Tcso*exp((Tcso-TcsI)/dTImC)-TcsI)/(exp((Tcso-TcsI)/dTImC)-1)
+    
+    T1=TE
+    
+    p1=PST(T1)
+    h1=HTDD(T1)
+    s1=STDD(T1)
+    
+    T3=TC
+    
+    p3=PST(T3)
+    h3=HTD(T3)
+    s3=STD(T3)
+    v3=VTD(T3)
+    
+    s2=s1
+    T2=T3
+    h21=HTD(T2)
+    h22=HTDD(T2)
+    s21=STD(T2)
+    s22=STDD(T2)
+    x2=(s2-s21)/(s22-s21)
+    h2=h21+x2*(h22-h21)
+    p4=p1
+    v4=v3
+    h4=h3+v4*(p4-p3)
+    eta=((h1-h2)-(h4-h3))/(h1-h4)
+    
+    mwf=Wt/((h1-h2)*1000)
+    cmwf=mwf
+    
+    Wp=mwf*(h4-h3)*1000
+    Qe=mwf*(h1-h4)*1000
+    Qc=mwf*(h2-h3)*1000
+    mws=Qe/(cp*(TwsI-Twso))
+    mcs=Qc/(cp*(Tcso-TcsI))
+    
+    end subroutine KDI1
